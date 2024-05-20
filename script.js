ddd9732161f950aaf97b5ce847d5fcb0afc291d9
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-20 17:47:47 +0200

    Add logic for round/game outcome

--- script.js
+++ script.js
@@ -2,29 +2,24 @@ function boardController() {
     // need to initialize board
         // board index is row
         // board index value is col
-    const boardCols = 3;
-    const boardRows = 3;
+    const boardCells = 9;
     const boardGrid = [];
 
     // create 2D array with nested array
-    for(let i = 0; i < boardCols; i++) {
-        boardGrid[i] = [];
-        for(let j = 0; j < boardRows; j++) {
-            // populate grid cells with objects / boardCell methods
-            boardGrid[i].push(boardCell());
-        }
+    for(let i = 0; i < boardCells; i++) {
+        boardGrid.push(boardCell());
     }
 
     // player plays the round in gameController, we have to update the board
-    const playerSelectCell = (selectedRow, selectedCol, player) => {
+    const playerSelectCell = (selectedCell, player) => {
         // first check if the selected cell was already selected in prev rounds
-        if(boardGrid[selectedRow][selectedCol].getCellValue() !== 0) {
+        if(boardGrid[selectedCell].getCellValue() !== 0) {
             // ignore the click
             console.log('not avail');
             return;
         } else {
             console.log('avail');
-            return boardGrid[selectedRow][selectedCol].updateCellValue(player);
+            return boardGrid[selectedCell].updateCellValue(player);
         }
     };
 
@@ -54,7 +49,7 @@ function gameController() {
         },
         {
             name: 'playerTwo',
-            value: 2,
+            value: -1,
         }
     ]
 
@@ -63,17 +58,44 @@ function gameController() {
 
     // get board
     const gameboard = boardController();
+    const getBoard = gameboard.getBoard();
+
+    // rounds counter
+    let counter = 0;
 
     // play round
-    const playRound = (selectedRow, selectedCol) => {
+    const playRound = (selectedCell) => {
         console.log(`${playerTurn['name']} is playing the round`);
 
-        if(!!gameboard.playerSelectCell(selectedRow, selectedCol, playerTurn['value']) === false) return;
+        if(!!gameboard.playerSelectCell(selectedCell, playerTurn['value']) === false) return;
 
-        playerTurn === players[0] ? playerTurn = players[1] : playerTurn = players[0];
-    }
+        const winPossibilities = [ 
+            [0, 1, 2], 
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [6, 4, 2]
+        ];
 
-    const getBoard = gameboard.getBoard;
+        const possibilitiesToValues = winPossibilities.map(function(scenario) {
+                return scenario.map(cell => cell = getBoard[cell].getCellValue());
+            });
+
+        const checkForOutcome = possibilitiesToValues.map(scenario => scenario.reduce((acc, item) => {
+            return acc + item;
+        }, 0));
+
+        if(checkForOutcome.includes(3 || -3) || counter === 9) {
+            return true;
+        }
+
+        counter++;
+
+        return false;
+    };
 
     return { playRound, getBoard };
 };
