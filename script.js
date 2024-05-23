442842b6f7ff410d0d5d42650b9a549da55d624b
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-23 15:10:09 +0200

    Finish UI version

--- script.js
+++ script.js
@@ -1,3 +1,89 @@
+(function domController() {
+    // initialize a new game
+    const game = gameController();
+
+    // create overlay
+    const overlay = document.querySelector('.overlay');
+    overlay.textContent = '';
+
+    createElement('.overlay', 'div', undefined, 'winner', 'hide');
+    createElement('.winner', 'p', 'Winner is', 'win-title', 'hide');
+    createElement('.winner', 'p', 'X', 'playerOne', 'hide');
+    createElement('.winner', 'p', 'O', 'playerTwo', 'hide');
+    createElement('.overlay', 'div', undefined, 'tie', 'hide');
+    createElement('.tie', 'p', 'Tie!', 'tie-title', 'hide');
+    createElement('.overlay', 'button', 'Restart', 'restart', 'hide');
+
+
+    // save board grid which is needed for appending buttons
+    const boardGrid = document.querySelector('.boardGrid');
+    boardGrid.textContent = '';
+
+    // create cells (buttons) and append them to boardGrid
+    for(let i = 0; i < 9; i++) {
+        const cellBtn = createElement('.boardGrid', 'button', undefined, 'cell');
+        cellBtn.dataset.index = i;
+    }
+
+    // save these buttons (cells)
+    const cells = document.querySelectorAll('.cell');
+
+    // add event listeners for these buttons
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+            const playerTurn = game.getPlayerTurn();
+
+            const roundSuccess = game.playRound(cell.dataset.index);
+            if(roundSuccess) {
+                playerTurn.value === 1 ? cell.textContent = 'X' : cell.textContent = 'O';
+                const outcome = game.checkOutcome();
+
+                if(outcome === 1) {
+                    toggleElement('.overlay');
+                    toggleElement('.winner');
+                    toggleElement('.win-title');
+                    toggleElement(`.${playerTurn.name}`);
+                    toggleElement('.restart');
+                } else if(outcome === 2) {
+                    toggleElement('.overlay');
+                    toggleElement('.tie');
+                    toggleElement('.tie-title');
+                    toggleElement('.restart');
+                }
+
+            }
+        })
+    })
+
+    // function for toggling outcome elements
+    function toggleElement(elementClass) {
+        const element = document.querySelector(elementClass);
+        element.classList.toggle('hide');
+    };
+
+    function createElement(parentElement, childElement, text = '', ...elementClass) {
+        const parent = document.querySelector(parentElement);
+        const child = document.createElement(childElement);
+        child.textContent = text;
+
+        for(let i = 0; i < elementClass.length; i++) {
+            child.classList.add(elementClass[i]);
+        }
+
+        parent.appendChild(child);
+
+        return child;
+    }
+
+    // restart button element & event listener
+    const restartBtn = document.querySelector('.restart');
+    return restartBtn.addEventListener('click', () => {
+        overlay.classList.toggle('hide');
+        return domController();
+    })
+})();
+
+
 function gameController() {
     // get boardController methods
     const board = boardController();
@@ -16,11 +102,10 @@ function gameController() {
 
     // initialize playerTurn to the first player
     let playerTurn = player[0];
+    const getPlayerTurn = () => playerTurn;
 
     // variable for outcome
     let outcome = 0;
-
-    console.log(`${playerTurn.name} turn`);
 
     // method for playing rounds, it takes cell number as an arg
     const playRound = (cellIndex) => {
@@ -29,10 +114,19 @@ function gameController() {
         
         // check if move was successful
         if(!moveSuccess) {
-            // if cell is already occupied, end the round and return false value
-            return moveSuccess;
+            // if cell is already occupied, end the round and 
+            //  return false = unsuccessful round & there's no playerTurn switch
+            return false;
         }
 
+        // if no condition is met, there's no outcome yet, swap playerTurn
+        playerTurn === player[0] ? playerTurn = player[1] : playerTurn = player[0];
+
+        // return true = successful round
+        return true;
+    }
+
+    const checkOutcome = () => {
         // check if there's a winner or a tie
         const winScenarios = [
             [0, 1, 2], 
@@ -62,33 +156,18 @@ function gameController() {
         // tie will check if all values are !== 0, it's impossible to be 0 if all 3 cells are occupied
         if(sumOfScenarioCells.includes(3) || sumOfScenarioCells.includes(-3)) {
             // 1 === winner
-            return checkOutcome(1);
+            return 1;
         } else if(!sumOfScenarioCells.includes(0)) {
             // 2 === tie
-            return checkOutcome(2);
-        }
-
-        // if no condition is met, there's no outcome yet, swap playerTurn
-        playerTurn === player[0] ? playerTurn = player[1] : playerTurn = player[0];
-
-        // 0 === no outcome
-        return checkOutcome(0);
-    }
-
-    // every round check outcome
-    function checkOutcome(outcome) {
-        if(outcome === 1) {
-            return console.log(`${playerTurn.name} won the game!`);
-        } else if(outcome === 2) {
-            return console.log('Game is tied!');
+            return 2;
         } else {
-            console.log('No winner yet');
-            return console.log(`${playerTurn.name} proceed with next round`);
+            // 0 === no outcome
+            return 0;
         }
     }
     
 
-    return { playRound };
+    return { playRound, checkOutcome, getPlayerTurn };
 }
 
 
